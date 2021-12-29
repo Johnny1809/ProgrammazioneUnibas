f41ce659e173d5e82f9644ed7874fbae9e1825fb
Johnny1809 <89039573+Johnny1809@users.noreply.github.com>
2021-12-29 19:16:37 +0100

    Rimosso il progetto Corrieri
    Rimosso il progetto Corrieri, che non ha nulla a che fare con il branch Develop-Isee

--- PrimoSemestre/Corrieri/Corrieri/app/src/main/java/it/unibas/corrieri/Costanti.java
+++
@@ -1,8 +0,0 @@
-package it.unibas.corrieri;
-
-public class Costanti {
-
-    public static String CORRIERI = "Corrieri";
-    public static String CORRIERE_SELEZIONATO = "Corriere Selezionato";
-
-}
--- PrimoSemestre/Corrieri/Corrieri/app/src/main/java/it/unibas/corrieri/controllo/ControlloDettagliCorriere.java
+++
@@ -1,9 +0,0 @@
-package it.unibas.corrieri.controllo;
-
-import android.view.View;
-
-public class ControlloDettagliCorriere {
-
-    //private View.OnClickListener
-
-}
